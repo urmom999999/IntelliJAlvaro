e93db876246d2c9955fd76d6b503e5b0fbb62490
Alvaro <alvarod@aulaestudio.es>
2025-04-10 15:55:30 +0200

    actualizacion

---
+++ Rectangulo/src/Main.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+public class Main {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        while (true) {
+            System.out.print("Ancho: ");
+            int ancho = scanner.nextInt();
+
+
+            System.out.print("Alto: ");
+            int alto = scanner.nextInt();
+
+
+            Rectangulo rectangulo = new Rectangulo(ancho, alto);
+            rectangulo.dibujar();
+        }}}
---
+++ Rectangulo/src/Rectangulo.java
@@ -0,0 +1,19 @@
+public class Rectangulo
+{int ancho;
+    int alto;
+
+
+    public Rectangulo(int ancho, int alto)
+    {this.ancho = ancho;
+        this.alto = alto;
+    }
+    //Doble for para alto ancho
+    public void dibujar() {
+        for (int i = 0; i < alto; i++) {
+            for (int j = 0; j < ancho; j++) {
+                System.out.print("■■");
+                //Alt 254
+            }
+            System.out.println();
+        }}}
+
